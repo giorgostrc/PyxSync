c2b47266c4ccf862bb67aecc007c4d8225d32efa
Giorgos Tritsarolis <giorgostrc@gmail.com>
2024-02-25 11:40:43 +0200

    Add get_date_range and copy_files

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import os
+import shutil
+from datetime import datetime
 from enum import Enum
 from typing import List, Optional
 
@@ -32,18 +34,26 @@ def detect_removable_storage() -> List[str]:
     return external_storage_devices
 
 
-def select_device(external_storage_devices: List[str]) -> Optional[str]:
-    if not external_storage_devices:
+def detect_local_storage() -> List[str]:
+    partitions = psutil.disk_partitions(all=True)
+    local_storage_devices = [
+        partition.device for partition in partitions if all(option in partition.opts for option in ["fixed", "rw"])
+    ]
+    return local_storage_devices
+
+
+def select_device(storage_devices: List[str]) -> Optional[str]:
+    if not storage_devices:
         print("No devices were found! Please connect an external storage!")
 
     print("External Devices:")
-    for i, device in enumerate(external_storage_devices):
+    for i, device in enumerate(storage_devices):
         print(f"{i}: {device}")
 
     try:
         selected_idx = int(input("Select a device id: "))
-        if selected_idx <= len(external_storage_devices):
-            selected_device = external_storage_devices[selected_idx]
+        if selected_idx <= len(storage_devices):
+            selected_device = storage_devices[selected_idx]
             print(f"Selected device {selected_idx}: {selected_device}")
             return selected_device
     except Exception as e:
@@ -81,16 +91,50 @@ def get_camera_model(filepath: str) -> str:
     if make in model:
         return model
     model = model.replace(make, "").strip(" ")
-    result = f"{make} {model}"
-    print(result)
-    return result
+    return f"{make} {model}"
+
+
+def get_photo_date_range(filepaths: List[str]) -> str:
+    dates = []
+    for filepath in filepaths:
+        f = open(filepath, "rb")
+        tags = exifread.process_file(f)
+        date_shot = tags.get("Image DateTime")
+        if date_shot:
+            date_shot = date_shot.values.split(" ")[0]
+            date_shot = datetime.strptime(date_shot, "%Y:%m:%d")
+            dates.append(date_shot)
+    dates = list(set(dates))
+    if len(dates) == 1:
+        return dates[0].strftime("%Y-%m-%d")
+    earliest_date = min(dates).strftime("%Y-%m-%d")
+    latest_date = max(dates).strftime("%Y-%m-%d")
+    return f"{earliest_date} - {latest_date}"
+
+
+def copy_files(filepaths: List[str], target_dir: str) -> None:
+    os.makedirs(target_dir, exist_ok=False)
+    # TODO: TQDM Progress update
+    for filepath in filepaths:
+        if os.path.isfile(filepath):
+            filename = os.path.basename(filepath)
+            destination = os.path.join(target_dir, filename)
+            shutil.copy2(filepath, destination)
+        else:
+            print(f"Warning: '{filepath}' is not a valid file path.")
 
 
 def main():
-    devices = detect_removable_storage()
-    selected_device = select_device(devices)
-    image_files = find_images(selected_device)
-    get_camera_model(image_files[0])
+    removable_storage_devices = detect_removable_storage()
+    source_device = select_device(removable_storage_devices)
+    source_files = find_images(source_device)
+    camera_model = get_camera_model(source_files[0])
+    date_range = get_photo_date_range(source_files)
+    local_storage_devices = detect_local_storage()
+    target_device = select_device(local_storage_devices)
+    target_dir = os.path.join(target_device, camera_model, date_range)
+    print(f"Destination path: {target_dir}")
+    copy_files(source_files, target_dir)
 
 
 if __name__ == "__main__":
